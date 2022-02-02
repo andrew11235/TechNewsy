6b24bdb87dcf9923ba73dda1c5186263ca895336
Andrew Wang <89869564+p4andrew23@users.noreply.github.com>
2022-02-01 23:38:36 -0800

    Update Scraper.py

--- CTNID Scraper/Scraper.py
+++ CTNID Scraper/Scraper.py
@@ -37,25 +37,35 @@ def generate_text():
     with open("TechNewsy.txt", "r", encoding="UTF-8") as corpus:
         gram_dict = dict(FreqDist(list(ngrams(corpus.read().split(), 3))))
 
-    for i in range(100):
-        while True:
-            starting = random.choice(list(gram_dict.keys()))
+    with open("PostList.txt", "a", encoding="UTF-8") as f:
+        f.write("[")
+        for i in range(500):
+            try:
+                while True:
+                    starting = random.choice(list(gram_dict.keys()))
 
-            if "." not in starting:
-                txt_list = [starting[i] for i in range(3)]
-                break
+                    if "." not in starting:
+                        txt_list = [starting[i] for i in range(3)]
+                        break
 
-        while len(txt_list) < 250 or txt_list[-1][-1] not in [".", "!", "?"]:
-            lis = {k[-1]: gram_dict[k] for k in gram_dict if k[0] == txt_list[-2] and k[1] == txt_list[-1]}
+                while len(txt_list) < 100 or txt_list[-1][-1] not in [".", "!", "?"]:
+                    lis = {k[-1]: gram_dict[k] for k in gram_dict if k[0] == txt_list[-2] and k[1] == txt_list[-1]}
 
-            next_word = random.choices(list(lis.keys()), list(lis.values()))[0]
+                    next_word = random.choices(list(lis.keys()), list(lis.values()))[0]
 
-            txt_list.append(next_word)
+                    txt_list.append(next_word)
 
-        body = " ".join(txt_list).capitalize()
-        body = ". ".join(map(lambda s: s.strip().capitalize(), body.split('.')))
+            except IndexError:
+                i -= 1
+                continue
 
-        print(body)
+            body = " ".join(txt_list).capitalize()
+            body = ". ".join(map(lambda s: s.strip().capitalize(), body.split('.')))
+            print(i)
+            print(body)
+
+            f.write(f', "{body}"' if i > 0 else f'"{body}"')
+        f.write("]")
 
 
 def main():
