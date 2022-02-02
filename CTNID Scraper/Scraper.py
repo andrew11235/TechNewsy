dc6b810de509bbb1dc3b0019884fdf413f528f20
Andrew Wang <89869564+p4andrew23@users.noreply.github.com>
2022-02-01 22:31:04 -0800

    Update Scraper.py

--- CTNID Scraper/Scraper.py
+++ CTNID Scraper/Scraper.py
@@ -1,10 +1,67 @@
+import random
 import requests
+from re import sub
 from bs4 import BeautifulSoup
+from nltk import FreqDist, ngrams
+
+
+def get_corpus():
+    link = "https://ctnid.blogspot.com/search?max-results=999"
+
+    with open("TechNewsy.txt", "w", encoding="UTF=8") as text_file:
+        while True:
+            try:
+                res = requests.get(link)
+                soup = BeautifulSoup(res.content, 'html.parser')
+                posts = soup.find_all("div", attrs={"class": 'post-body'})
+
+                for p in posts:
+                    body = ""
+                    p = p.text.split("\n")
+                    for line in p:
+                        if not (line.startswith("https:") or line.startswith("Credit")):
+                            line = sub(r"http\S+", "", line)
+                            line = sub(r"http\S+", "", line)
+                            line = sub(r"\.(?=[\S])", ". ", line)
+                            line = line.replace('"', "").replace('(', "").replace(')', "")
+                            body += line
+                    text_file.write(body + "\n")
+
+                link = soup.find("a", attrs={'class': 'blog-pager-older-link'})['href']
+                print(link)
+            except (TypeError, ConnectionError):
+                break
+
+
+def generate_text():
+    with open("TechNewsy.txt", "r", encoding="UTF-8") as corpus:
+        gram_dict = dict(FreqDist(list(ngrams(corpus.read().split(), 3))))
+
+    for i in range(100):
+        while True:
+            starting = random.choice(list(gram_dict.keys()))
+
+            if "." not in starting:
+                txt_list = [starting[i] for i in range(3)]
+                break
+
+        while len(txt_list) < 250 or txt_list[-1][-1] not in [".", "!", "?"]:
+            lis = {k[-1]: gram_dict[k] for k in gram_dict if k[0] == txt_list[-2] and k[1] == txt_list[-1]}
+
+            next_word = random.choices(list(lis.keys()), list(lis.values()))[0]
+
+            txt_list.append(next_word)
+
+        body = " ".join(txt_list).capitalize()
+        body = ". ".join(map(lambda s: s.strip().capitalize(), body.split('.')))
+
+        print(body)
+
 
 def main():
-  pass
-  
-  
-  
-if __name__ == "__main__":
-  main()
+    get_corpus()
+    generate_text()
+
+
+if __name__ == '__main__':
+    main()
