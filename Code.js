1d3f3b49eb1b29ea843e774e031e95bc3ebc09d8
Andrew Wang <89869564+p4andrew23@users.noreply.github.com>
2022-02-02 19:44:51 -0800

    Delete Code.js

--- Code.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-var list = ["1", "2"];
-var rand = Math.floor(Math.random() * list.length);
-
-document.getElementById("newsnum").innerHTML = "Newsy " + rand + ": ";
-
-document.getElementById("text").innerHTML = list[rand].toString();
-}
